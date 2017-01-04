5a895f4bb3493bf78267b9ce8cdd89b645f49fdb
PowerUser <PowerUser>
2017-01-04 13:13:24 +0600

    first commit

---
+++ DataTables/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataTables.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String productName { get; set; }
+        public byte[] productImage { get; set; }
+        public int productQuantity { get; set; }
+    }
+}
